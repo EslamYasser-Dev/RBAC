c5d84b27dad6e4eee702b63c46ccd305a44d653d
Eslam <e.yasser997@gmail.com>
2024-08-01 15:36:47 +0300

    init commit

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,24 @@
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core"
+import { pgTable, primaryKey, timestamp, uniqueIndex, uuid, varchar } from "drizzle-orm/pg-core"
 
-export const application = - pgTable("applications", {
+export const applications = pgTable("applications", {
     id: uuid("id").primaryKey().defaultRandom(),
     name: varchar("name", { length: 256 }).notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     UpdatedAt: timestamp('Updated_at').defaultNow().notNull(),
-    
+});
 
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    email: varchar("email", { length: 256 }).notNull(),
+    name: varchar("name", { length: 256 }).notNull(),
+    applicationId: uuid("applicationId").references(() => applications.id),
+    password: varchar("password", { length: 256 }).notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    UpdatedAt: timestamp('Updated_at').defaultNow().notNull(),
+}, (users) => {
+    return {
+        cpk: primaryKey(users.email, users.applicationId),
+        idIndex: uniqueIndex('users_id_index').on(users.id),
+    }
 })
+
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { env } from "./config/env";
 import { logger } from "./utils/logger";
 import { buildServer } from "./utils/server";
-
+import { db } from './db';
 
 async function shutDownServer({
     app,
@@ -13,7 +14,9 @@ async function shutDownServer({
 async function main() {
     const app = await buildServer();
     app.listen({ port: env.PORT, host: env.HOST });
-
+    await migrate(db, {
+        migrationsFolder: "./migrations",
+    });
     //log for closing server
     const signals = ['SIGINT', 'SIGTREM'];
     logger.debug(env);
